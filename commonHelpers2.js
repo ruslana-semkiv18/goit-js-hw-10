02dc0e0570e8ca65d04b7d9595072274ebeed896
ruslana-semkiv18 <156903697+ruslana-semkiv18@users.noreply.github.com>
2024-07-12 18:43:31 +0000

    Deploying to gh-pages from @ ruslana-semkiv18/goit-js-hw-10@c3ab2ee7aacfe53046e9ea55178fc77c8d2ff321 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import"./assets/vendor-77e16229.js";const e=document.querySelector("button");document.querySelector('input[type="number"]');document.querySelectorAll('input[type="radio"]');e.addEventListener("click",t);function t(){}
+//# sourceMappingURL=commonHelpers2.js.map

c3ab2ee7aacfe53046e9ea55178fc77c8d2ff321
ruslana-semkiv18 <semkivruslana18@gmail.com>
2024-07-12 21:43:08 +0300

    update snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,13 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const btnCreate = document.querySelector("button");
+const inputDelay = document.querySelector('input[type="number"]');
+const inputsState = document.querySelectorAll('input[type="radio"]');
+
+
+btnCreate.addEventListener("click", handlerCreate);
+
+function handlerCreate() {
+    
+}
